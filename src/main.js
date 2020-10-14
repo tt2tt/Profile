ed30f3cfdc4a63967705e0272dce08fae1cefac0
”taro <groveleffect@gmail.com>
2020-10-15 07:44:50 +0900

    Deploy To Firebase

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserCircle,faFolder,faLaptopCode,faEnvelope,faHome,faGlobe,faBlog,faBirthdayCake,faImage } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter,faFacebook,faGithub,faHtml5,faCss3Alt,faJsSquare } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import firebase from 'firebase'
 
 Vue.use(VueParticles)
 Vue.use(VueTyperPlugin)
@@ -19,6 +20,20 @@ library.add(faUserCircle,faFolder,faLaptopCode,faEnvelope,faHome,faGlobe,faBlog,
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBkDmASmU-I8hvL85lP6bl_WF9BuYlm1yo",
+  authDomain: "tt2tt-profile-page.firebaseapp.com",
+  databaseURL: "https://tt2tt-profile-page.firebaseio.com",
+  projectId: "tt2tt-profile-page",
+  storageBucket: "tt2tt-profile-page.appspot.com",
+  messagingSenderId: "1086215052435",
+  appId: "1:1086215052435:web:ff9a469c60367e35570166",
+  measurementId: "G-GN2EQXJL9H"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   router,
